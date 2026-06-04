966fadc5ae1cbb90d1530c1206c05542f839201b
John Carlo Namor <carlokun1999@gmail.com>
2026-06-04 11:10:02 +0800

    feat: add timeline-dot Blade component for timeline markers
    Colored ringed dot for vertical timeline event markers.
    - Accepts color class prop for different event types
    - White ring around colored dot for visual separation
    - Used in file timeline view for each event

---
+++ resources/views/components/timeline-dot.blade.php
@@ -0,0 +1,5 @@
+@props(['color' => 'bg-gray-400'])
+
+<div class="relative flex items-center justify-center">
+    <div class="w-4 h-4 rounded-full {{ $color }} ring-4 ring-white dark:ring-gray-900 z-10"></div>
+</div>
